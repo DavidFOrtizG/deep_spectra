350eed3ab859c3e3fbb1dece632ab93580ef4117
David Ortiz <david.f.ortizg@gmail.com>
2025-11-16 20:27:00 -0500

    Adding some absortion lines to the constants

--- src/physics/constants.py
+++ src/physics/constants.py
@@ -1,6 +1,16 @@
-h = 6.62607015e-34 #Constante de Planck (J·s)
-c = 2.99792458e8 #Velocidad de la luz (m/s)
-k_B = 1.380649e-23 #Constante de Boltzmann (J/K)
+# Physical constants
+h = 6.62607015e-34 # Planck constant (J·s)
+c = 2.99792458e8 #Speed of light (m/s)
+k_B = 1.380649e-23 #Boltzmann constant (J/K)
+
+# Absortion lines (m)
+H_alpha = 656.21e-9
+H_beta = 486.08e-9
+H_gamma = 434.00e-9
+H_delta = 410.13e-9
+Ca_H = 396.847e-9
+Ca_K = 393.366e-9
+Mg_b = 517.5e-9
 
 
 if __name__ == "__main__":
