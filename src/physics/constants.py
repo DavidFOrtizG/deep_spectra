6bc382bd7adb7d13b9020695bcab3c08e5dcb489
David Ortiz <david.f.ortizg@gmail.com>
2025-11-16 20:02:25 -0500

    Physical constants defined

---
+++ src/physics/constants.py
@@ -0,0 +1,7 @@
+h = 6.62607015e-34 #Constante de Planck (J·s)
+c = 2.99792458e8 #Velocidad de la luz (m/s)
+k_B = 1.380649e-23 #Constante de Boltzmann (J/K)
+
+
+if __name__ == "__main__":
+    pass
